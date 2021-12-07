0267461d709b9d61513eae9085ebd560c38e0d07
Gedeon Eyasu <gedyeyasu@gmail.com>
2021-12-07 23:07:28 +0300

    finished setting up backend api

---
+++ backend/products.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    id: 1,
+    name: "iphone 12 pro",
+    desc: "6.1 inch display",
+    price: 699,
+    image: "https://m.media-amazon.com/images/I/71MHTD3uL4L._FMwebp__.jpg",
+  },
+  {
+    id: 1,
+    name: "iphone 10 pro",
+    desc: "5.1 inch display",
+    price: 599,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81LmL94PUvS.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  },
+  {
+    id: 1,
+    name: "imac pro",
+    desc: "34.7 inch display",
+    price: 799,
+    image:
+      "https://m.media-amazon.com/images/I/71z+iYqulHL._AC_SY300_SX300_.jpg",
+  },
+  {
+    id: 1,
+    name: "Macbook 12 pro",
+    desc: "16.1 inch display",
+    price: 499,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51pWDiO9CrL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  },
+];
+
+module.exports = products;
