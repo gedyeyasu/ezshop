7ee55816947d2dd68102a63bc4c4d82ce156db3c
Gedeon Eyasu <gedyeyasu@gmail.com>
2021-12-07 22:37:18 +0300

    finished creating navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import NotFound from "./components/NotFound";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NotFound.css";
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h2>Not Found</h2>
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default NotFound;
